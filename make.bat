aedfd4a195d76b3e6f235c5f3315ff356e85988a
Bjoern Schwabe <bjoern.schwabe@jumpsec.com>
2024-07-31 15:08:42 +0300

    Updated metadata formatting

--- make.bat
+++ make.bat
@@ -2,4 +2,4 @@ cd docs
 
 pandoc $(cat pandoc_order.txt) metadata.yaml --include-before-body=version.md -s -o developer_handbook.html --toc --number-sections --wrap=none
 
-pandoc docs/developer_handbook.html -s --pdf-engine=xelatex -o docs/developer_handbook.pdf
+pandoc developer_handbook.html -s --pdf-engine=xelatex -o developer_handbook.pdf
