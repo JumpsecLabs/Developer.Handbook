8630feae84cefc27aa976b156624fd91947fdf38
Bjoern Schwabe <bjoern.schwabe@jumpsec.com>
2024-07-31 14:45:40 +0300

    hardcoded file names

--- make.bat
+++ make.bat
@@ -1,5 +1,5 @@
-mkdir dist
+cd docs 
 
-pandoc $(cat pandoc_order.txt) metadata.yaml --include-before-body=version.md -s -o dist/developer_handbook.html --toc --number-sections --wrap=none
+pandoc $(cat pandoc_order.txt) metadata.yaml --include-before-body=version.md -s -o developer_handbook.html --toc --number-sections --wrap=none
 
-pandoc dist/developer_handbook.html -s --pdf-engine=xelatex -o dist/developer_handbook.pdf
+pandoc docs/developer_handbook.html -s --pdf-engine=xelatex -o docs/developer_handbook.pdf
